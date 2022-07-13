5969adef2413ae1d28a6ab33c8206ea8c6bc32b9
qhtLucifer <qhtLucifer@foxmail.com>
2022-07-13 23:30:39 +0800

    Update config.py

---
+++ utils/config.py
@@ -0,0 +1,13 @@
+import argparse
+def parse_args():
+
+    # ===============================Project ============================
+    parser = argparse.ArgumentParser(description='Arguments for person pose estimation.')
+    parser.add_argument('--seed',type = int, default=7310,help = 'Random seed.')
+    
+
+    # ===============================Data ================================
+
+    
+
+    return parser
