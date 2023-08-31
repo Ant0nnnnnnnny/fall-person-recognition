e6c43fb53c5ad37fe95cb17aa6fb53e0a3064fae
qhtLucifer <qhtLucifer@foxmail.com>
2023-08-31 08:49:25 +0800

    Fix bugs

--- configs/sgn.py
+++ configs/sgn.py
@@ -2,7 +2,7 @@ import os
 def sgn_config(parser):
     parser.add_argument('--num_classes', type=int,
                         default=1)
-    parser.add_argument('--seg', type = int ,default = 50)
+    parser.add_argument('--seg', type = int ,default = 30)
     parser.add_argument('--bias', type=bool, default=True) 
     parser.add_argument('--num_joint', type = int, default = 17)
     parser.add_argument('--channels', type = int, default = 2)
